9889d3f3cd50bdea123c6a54654a7fcf1a04bcd1
Ashley Ingram <ashley2504@hotmail.com>
2012-04-24 16:00:33 +0100

    Olympics Site  + comments now display on the video page  - removed some debug code from the custom tags

--- VideoSite/templatetags/tags.py
+++ VideoSite/templatetags/tags.py
@@ -5,7 +5,6 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def active(context, pattern):
     try:
-        print context['sport']
         if context['sport'].name == pattern:
             return 'active'
     except:
